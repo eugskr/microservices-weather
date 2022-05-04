cd6a63298819a6a23ef9e732ca0d3ea6100bf6c0
Yevhen Skrypnyk <y.skrypnyk@spd-ukraine.com>
2022-05-04 22:13:55 +0200

    add data access to precipitation service

---
+++ CloudWeather.Precipitation/DataAccess/PrecipDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CloudWeather.Precipitation.DataAccess;
+
+public class PrecipDbContext: DbContext
+{
+    public PrecipDbContext(DbContextOptions opt): base(opt)
+    {
+        
+    }
+    
+    public DbSet<Precipitation> Precipitations { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        SnakeCaseIdentityTablesNames(modelBuilder);
+        
+    }
+
+    private static void SnakeCaseIdentityTablesNames(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Precipitation>(b => { b.ToTable("precipitation"); });
+    }
+}
--- CloudWeather.Precipitation/Program.cs
+++ CloudWeather.Precipitation/Program.cs
@@ -1,5 +1,29 @@
+using CloudWeather.Precipitation.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<PrecipDbContext>(opt =>
+{
+    opt.EnableSensitiveDataLogging();
+    opt.EnableDetailedErrors();
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("AppDb"));
+}, ServiceLifetime.Transient);
+
 var app = builder.Build();
+
+app.MapGet("/observation/{zip}", async (string zip, [FromQuery] int? days, PrecipDbContext db) =>
+{
+    if (days is null or < 1 or > 30)
+    {
+        return Results.BadRequest("Please provide a 'days' query parameters between 1 and 30");
+    }
+
+    var startDate = DateTime.UtcNow - TimeSpan.FromDays(days.Value);
+    var result = await db.Precipitations.Where(precip => precip.ZipCode == zip && precip.CreatedOn > startDate)
+        .ToListAsync();
+    return Results.Ok(result);
+});
 
 app.Run();
